b0d73dcdc2e89b9ff88dd15a4ab54ba05aae20f7
Your Name <you@example.com>
2025-05-09 14:18:07 +0000

    Add and configure CORS middleware with environment-based origins

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const corsMiddleware = require('./middleware/cors');
 const { Sequelize, Op } = require('sequelize');
 const path = require('path');
 const bodyParser = require('body-parser');
